774ff611eefd8f047ef43961331c90a52ff9776a
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2022-04-27 19:26:38 +0100

    redirect via meta tags

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['github.com'],
+  },
 }
 
 const withYAML = require('next-yaml')
--- pages/donate.js
+++ pages/donate.js
@@ -1,22 +1,22 @@
 import React, { useEffect } from "react"
-import Head from 'next/head'
-import mosques from '../data/mosques.yml'
+import Head from "next/head"
+import Image from "next/image"
+import mosques from "../data/mosques.yml"
 
 function Donate(props) {
-  useEffect(() => {
-    if (props.data.donation) window.location.href = props.data.donation.link;
-  }, []);
-
   return (
     <>
       <Head>
         <title>Donate to {props.data.name}</title>
+        {props.data.donation && (<meta http-equiv="refresh" content={`0;url=${props.data.donation.link}`} />)}
       </Head>
-      <div className="mx-auto py-10 px-4 max-w-7xl sm:px-6 lg:px-8 items-center">
-        <img
-          className="h-16 w-auto mx-auto"
+      <div className="mx-auto py-10 px-4 max-w-7xl sm:px-6 lg:px-8 text-center">
+        <Image
+          height={64}
+          width={156}
           src={props.data.branding.logo_url}
-        />
+          alt={`${props.data.name} logo`}
+        ></Image>
         <h1 className="mx-auto w-auto text-lg text-center my-5">Redirecting you to the donation page...</h1>
       </div>
     </>
