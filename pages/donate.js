bbf79312ca3e06f804192c2934f13b5466d1f51d
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2022-04-27 01:53:21 +0100

    add logo

--- pages/donate.js
+++ pages/donate.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"
+import Head from 'next/head'
 import mosques from '../data/mosques.yml'
 
 function Donate(props) {
@@ -7,9 +8,18 @@ function Donate(props) {
   }, []);
 
   return (
-    <div>
-      <h1>Redirecting you to the donation page</h1>
-    </div>
+    <>
+      <Head>
+        <title>Donate to {props.data.name}</title>
+      </Head>
+      <div className="mx-auto py-10 px-4 max-w-7xl sm:px-6 lg:px-8 items-center">
+        <img
+          className="h-16 w-auto mx-auto"
+          src={props.data.branding.logo_url}
+        />
+        <h1 class="mx-auto w-auto text-lg text-center my-5">Redirecting you to the donation page...</h1>
+      </div>
+    </>
   );
 }
 
