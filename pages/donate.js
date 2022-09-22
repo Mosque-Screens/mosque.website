a1c84dc1584268a7d625b7102462e862fcf6e304
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2022-09-22 23:31:05 +0100

    donation link added

--- pages/donate.js
+++ pages/donate.js
@@ -8,15 +8,13 @@ function Donate(props) {
     <>
       <Head>
         <title>Donate to {props.data.name}</title>
-        {props.data.donation && (<meta httpEquiv="refresh" content={`0;url=${props.data.donation.link}`} />)}
+        {props.data.donation && (<meta httpEquiv="refreshxxx" content={`0;url=${props.data.donation.link}`} />)}
       </Head>
       <div className="mx-auto py-10 px-4 max-w-7xl sm:px-6 lg:px-8 text-center">
-        <Image
-          height={64}
-          width={156}
+        <img
+          className={`${props.data.branding.larger_logo ? "h-32" : "h-16"} w-auto mx-auto`}
           src={props.data.branding.logo_url}
-          alt={`${props.data.name} logo`}
-        ></Image>
+        />
         <h1 className="mx-auto w-auto text-lg text-center my-5">Redirecting you to the donation page...</h1>
       </div>
     </>
